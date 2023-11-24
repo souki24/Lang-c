6f2f939d7cc97b42c9335b9a42f240a9abbd4d83
souki <s0224.com>
2023-11-24 14:26:28 +0900

    3

---
+++ 3/for_test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    for (int i = 0; i <= 19; i++) {
+        printf("%d\n", i);
+    }
+return 0; }
---
+++ 3/while_test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int count=1;
+    while (count <= 5) {
+        printf("%d\n", count*2);
+count++; }
+return 0; }
